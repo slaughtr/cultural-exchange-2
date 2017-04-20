8ded70847fa075852d6e43d265a6c5cf8c17aeb4
slaughtr <dslaughtr@gmail.com>
2017-04-20 11:36:08 -0700

    Have promises working somewhat, lots of console logs. Lots of bugs, too.

--- js/exchange-interface.js
+++ js/exchange-interface.js
@@ -1,9 +1,6 @@
-
 const getCurrencyAbbreviation = require('./../node_modules/country-currency-map').getCurrencyAbbreviation;
 const getExchange = require('./../js/exchange-interface.js').getExchange;
-const exchangeRatePlease = require('./../js/calculator.js').exchangeRatePlease;
-const calculate = require('./../js/calculator.js').calculate;
-
+const testPromise = require('../js/exchange.js').testPromise;
 
 let countryName1;
 let homeAbbv;
@@ -13,6 +10,7 @@ let income;
 let expenses;
 let timeLength;
 let exchangeRate;
+const inputArray = []
 
 
 $(function() {
@@ -20,28 +18,35 @@ $(function() {
     // debugger;
     event.preventDefault();
     countryName1 = $('#country1Input').val();
+    inputArray.push(countryName1) //0
     // console.log('countryName1 : '+countryName1);
     homeAbbv = getCurrencyAbbreviation(countryName1);
+    inputArray.push(homeAbbv) // 1
     // console.log('homeAbbv : '+homeAbbv);
     countryName2 = $('#country2Input').val();
+    inputArray.push(countryName2) //2
     // console.log('countryName2 : '+countryName2);
     destAbbv = getCurrencyAbbreviation(countryName2);
+    inputArray.push(destAbbv)//3
     // console.log('destAbbv : '+destAbbv);
     income = parseInt($('#income').val());
+    inputArray.push(income)//4
     // console.log('income:  '+income);
     expenses = parseInt($('#expenses').val());
+    inputArray.push(expenses)//5
     // console.log('expenses : '+expenses);
     timeLength = parseInt($('#timeLength').val());
+    inputArray.push(timeLength)//6
     // console.log('timeLength: '+timeLength);
-    exchangeRate = parseFloat(exchangeRatePlease(countryName1, countryName2));
     // console.log('exchangeRate : '+exchangeRate);
-    function doStuff() {
-      Promise.all(exchangeRatePlease(countryName1, countryName2), getExchange(countryName1, homeAbbv, countryName2, destAbbv, exchangeRate)).then(function() {
-      calculate(income, expenses, timeLength, countryName1, countryName2, exchangeRate)
-      console.log('did stuff')
-    })
-  };
-  doStuff()
-    // console.log('calculate : '+  calculate(income, expenses, timeLength, countryName1, countryName2, exchangeRate));
-  })
-});
+
+    // Promise.all(testPromise(income, expenses, timeLength, homeAbbv, destAbbv)).then(function(stuff) {
+    //   console.log(stuff);
+    // })
+    // testPromise(homeAbbv, destAbbv, inputArray)
+    testPromise(income, expenses, timeLength, homeAbbv, destAbbv, countryName1, countryName2)
+})
+})
+
+
+// exports.inputArray = inputArray
--- js/exchange.js
+++ js/exchange.js
@@ -11,46 +11,35 @@ let exchangeRateArray = []
 let destAbbv
 let homeAbbv
 
-function exchangeRatePlease(countryName1, countryName2) {
-  // debugger;
-  homeAbbv = getCurrencyAbbreviation(countryName1);
-  destAbbv = getCurrencyAbbreviation(countryName2);
+let testPromise = function(income, expenses, timeLength, homeAbbv, destAbbv, homeCountry, destCountry) {
   return new Promise(function(resolve, reject) {
-    $.get('http://api.fixer.io/latest?base='+homeAbbv+'&symbols='+homeAbbv+','+destAbbv).then(function(response2) {
-    exchangeRateArray.push(parseFloat(Object.values(response2.rates)[0]))
-    console.log('response2.rates[0]: '+parseFloat(Object.values(response2.rates)[0]))
-  }).fail(function(error) {
-    console.log("exchangeRatePlease failed")
+    $.get('http://api.fixer.io/latest?base='+homeAbbv+'&symbols='+homeAbbv+','+destAbbv)
+  .then(function(xchangeRate) {
+    let exchangeRate = Object.values(xchangeRate.rates)[0]
+    console.log('inside the testPromise');
+    calculate(income, expenses, timeLength, homeCountry, destCountry, parseFloat(exchangeRate).toFixed(3))
+    return exchangeRate
+    });
   })
-  // return exchangeRateArray[0]
-  console.log('exchangeRateArray[0]: '+exchangeRateArray[0])
-})
 }
 
 function calculate(income, expenses, timeLength, homeCountry, destCountry, exchangeRate) {
   costConvert = ((costOfLiving[destCountry])/(costOfLiving[homeCountry])).toFixed(2)
   console.log('costConvert : '+costConvert);
   netPerMonthAtHome = ((income/12)-expenses).toFixed(2)
-  console.log('netPerMonthAtHome : '+netPerMonthAtHome);
+  // console.log('netPerMonthAtHome : '+netPerMonthAtHome);
   neededPerMonthForTrip = ((expenses / exchangeRate) / costConvert).toFixed(2)
   console.log(' neededPerMonthForTrip : '+neededPerMonthForTrip);
+
   saveTimeForTrip = ((neededPerMonthForTrip/exchangeRate)/netPerMonthAtHome).toFixed(2)
   console.log('saveTimeForTrip : ' + saveTimeForTrip);
   spendingMoneyPerMonthOnTrip = ((expenses / exchangeRate) / costConvert).toFixed(2)
   console.log('spendingMoneyPerMonthOnTrip : '+ spendingMoneyPerMonthOnTrip);
   totalAmountNeededForTrip = (neededPerMonthForTrip * timeLength).toFixed(2)
   console.log('totalAmountNeededForTrip : '+ totalAmountNeededForTrip);
+  console.log('=-=-=-=-=-=-=-=-=-=-=');
   // console.log(exchangeRate, costConvert, netPerMonthAtHome, neededPerMonthForTrip, saveTimeForTrip, spendingMoneyPerMonthOnTrip, totalAmountNeededForTrip)
 }
 
-function getExchange(homeCountry, homeAbb, destCountry, destAbb, exchangeRate) {
-  return $.get('http://api.fixer.io/latest?base='+homeAbb+'&symbols='+homeAbb+','+destAbb).then(function(response) {
-    $('#infoBox').text('There are '+exchangeRate+' '+destCountry+' to every '+homeCountry);
-  }).fail(function(error) {
-    $('#infoBox').text('There was an error');
-  });
-}
 
-
-exports.calculate = calculate;
-exports.exchangeRatePlease = exchangeRatePlease;
+exports.testPromise = testPromise;
